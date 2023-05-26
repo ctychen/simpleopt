c35820ad2709f8b38b604fd6b6a2ea5b14b03f60
Claire Chen <ctychen@berkeley.edu>
2023-05-26 09:43:15 -0400

    why error with no cadclass?

--- src/Solid.py
+++ src/Solid.py
@@ -1,9 +1,9 @@
-from CADClass import CAD
+import CADClass
 
-class Box(CAD): 
+class Box(CADClass.CAD): 
     
     def __init__(self, stpfile):
-        super(CAD, self).__init__()
+        super(CADClass.CAD, self).__init__()
         self.STPfile = stpfile #want to be able to use as many HEAT cadclass fcns as possible, is this how to do it?
         return
 
--- src/run.py
+++ src/run.py
@@ -8,14 +8,55 @@ sys.path = [FreeCADPath]
 
 import CADClass
 
-# from Solid import Box
-# from ForwardModel_Box import ForwardModel
-# from OptModel_Box import OptModel  
 import Solid
-import ForwardModel_Box
-import OptModel_Box
+import ForwardModel
+import OptModel
+
+class RunSetup:
+    def __init__(self):
+        g_obj = lambda qvals: np.maximum(qvals)
+
+        stpPath = "test.stp"
+        qDirIn = [0, 1, 0] #[m]
+        qMagIn = 10 #[W/m^2]
+
+        self.box = Solid.Box(stpPath)
+        self.fwd = ForwardModel.ForwardModel_Box(g_obj, box, qMagIn, qDirIn) 
+        self.opt = OptModel.OptModel_Box()
+
+        self.box.loadSTEP()
+
+        self.del_theta = 0
+        return
+
+    def runModel(self):
+        while (self.opt.del_e > self.opt.threshold_err):
+            self.fwd.processCADModel()
+            q_mesh_all = self.fwd.calcQMesh()
+            g_now = self.fwd.calcObjective(q_mesh_all)
+            self.opt.updategValues(g_now)
+            del_err = self.opt.calculateDelE()
+
+            if (del_err > self.opt.threshold_err):
+                print(f"found opt, last err: {del_err}, rotated: {self.del_theta}")
+                break
+            else: 
+                self.del_theta = opt.doTransform(box) #this prob doesn't match yet, gonna fix
+                print(f"transformed: [err]: {del_err} [g_x now]: {self.opt.g_curr} [g_x prev]: {self.opt.g_prev} [theta]: {self.del_theta}")
+            
+            return
+        
+
+
 
 if __name__ == '__main__':
+
+    t0 = time.time()
+
+    setup = RunSetup()
+    setup.runModel()
+
+    print(f"Time elapsed: {time.time() - t0}")
 
     #set up scenario
     #read config and get initial values/models
@@ -32,19 +73,19 @@ if __name__ == '__main__':
     #log time for vibes  
 
     #objective function
-    g_obj = lambda qvals: np.maximum(qvals)
+    # g_obj = lambda qvals: np.maximum(qvals)
 
-    stpPath = "test.stp"
-    qDirIn = [0, 1, 0] #[m]
-    qMagIn = 10 #[W/m^2]
+    # stpPath = "test.stp"
+    # qDirIn = [0, 1, 0] #[m]
+    # qMagIn = 10 #[W/m^2]
 
-    box = Box(stpPath)
-    fwd = ForwardModel(g_obj, box, qMagIn, qDirIn) 
-    opt = OptModel()
+    # box = Solid.Box(stpPath)
+    # fwd = ForwardModel.ForwardModel_Box(g_obj, box, qMagIn, qDirIn) 
+    # opt = OptModel.OptModel_Box()
 
-    t0 = time.time()
+    # t0 = time.time()
 
-    box.loadSTEP()
+    # box.loadSTEP()
 
     # while (del_e > threshold_err): 
     #     q = fwd.calculateQ()
@@ -56,19 +97,19 @@ if __name__ == '__main__':
     #     else: 
     #         box = box.manipulate() #modify it, should probably specify how eventually
 
-    while (opt.del_e > opt.threshold_err):
-        fwd.processCADModel()
-        q_mesh_all = fwd.calcQMesh()
-        g_now = fwd.calcObjective(q_mesh_all)
-        opt.updategValues(g_now)
-        del_e = opt.calculateDelE()
+    # while (opt.del_e > opt.threshold_err):
+    #     fwd.processCADModel()
+    #     q_mesh_all = fwd.calcQMesh()
+    #     g_now = fwd.calcObjective(q_mesh_all)
+    #     opt.updategValues(g_now)
+    #     del_e = opt.calculateDelE()
 
-        if (del_e > opt.threshold_err):
-            print(f"found opt, time elapsed: {time.time() - t0}, err: {opt.del_e}")
-            break
-        else: 
-            del_theta = opt.doTransform(box) #this prob doesn't match yet, gonna fix
-            print(f"transformed: [err]: {opt.del_e} [g_x now]: {opt.g_curr} [g_x prev]: {opt.g_prev} [theta]: {del_theta}")
+    #     if (del_e > opt.threshold_err):
+    #         print(f"found opt, time elapsed: {time.time() - t0}, err: {opt.del_e}")
+    #         break
+    #     else: 
+    #         del_theta = opt.doTransform(box) #this prob doesn't match yet, gonna fix
+    #         print(f"transformed: [err]: {opt.del_e} [g_x now]: {opt.g_curr} [g_x prev]: {opt.g_prev} [theta]: {del_theta}")
 
 
 
